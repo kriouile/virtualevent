613afb530e56b25ab07954b7628ee43b5f1f1770
kriouile <44243940+kriouile@users.noreply.github.com>
2021-02-17 22:25:50 +0100

    Update constants.js

--- src/Utils/constants.js
+++ src/Utils/constants.js
@@ -1,3 +1,3 @@
-export const VEERTLY_MUTE_AUDIO_STATES = "veertly/mute_audio_state"
-export const VEERTLY_MUTE_VIDEO_STATES = "veertly/mute_video_state"
-export const SHOW_TECH_CHECK_LOCAL_STORAGE = "veertly/show_tech_check";
+export const VEERTLY_MUTE_AUDIO_STATES = "hadath/mute_audio_state"
+export const VEERTLY_MUTE_VIDEO_STATES = "hadath/mute_video_state"
+export const SHOW_TECH_CHECK_LOCAL_STORAGE = "hadath/show_tech_check";
